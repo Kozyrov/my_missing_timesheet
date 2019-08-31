d92de55fa7b65a26ddb211e89aaa64299cb34dc8
Steven Frankenfield <Stevenf@magenic.com>
2019-08-31 02:48:06 -0500

    +input fields

--- src/Components/TimeEntryContainer/TimeEntryContainer.js
+++ src/Components/TimeEntryContainer/TimeEntryContainer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './timeEntryContainer.css';
+import { TimeInput } from '../TimeInput/TimeInput';
 
 export const TimeEntryContainer = props =>
     <div className='time-entry-container'>
-        time
+        <TimeInput />
     </div>
---
+++ src/Components/TimeInput/TimeInput.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import './timeInput.css';
+
+export const TimeInput = props => <input type='text' placeholder='#' className='time-input'></input>
