b919560c8a9744e76a3bf638c33ed33e22d873b1
Steven Frankenfield <Stevenf@magenic.com>
2019-09-12 10:27:23 -0500

    changed result default state

--- src/Reducers/Result.js
+++ src/Reducers/Result.js
@@ -2,11 +2,8 @@ import {
     SET_CURRENT_TIMESHEET,
     CREATE_TIMESHEET
 } from "../Actions/ActionTypes";
-import initialState from './State';
 
-const initalResult = initialState.result;
-
-const result = (state = initalResult, action) => {
+const result = (state, action) => {
     switch (action.type) {
         case SET_CURRENT_TIMESHEET:
             return action.payload.result;
